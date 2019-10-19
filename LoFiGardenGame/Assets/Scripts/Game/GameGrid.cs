7e3a07cedd642ff4e4a0446701812c5dc0f84e40
Eliot Cowley <eliot93@live.com>
2019-10-19 14:30:56 -0700

    Added grid

---
+++ LoFiGardenGame/Assets/Scripts/Game/GameGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Grid))]
+public class GameGrid : MonoBehaviour
+{
+    [SerializeField]
+    private int rows = 4;
+
+    [SerializeField]
+    private int columns = 4;
+
+    private Grid grid;
+
+    private void Start()
+    {
+        grid = GetComponent<Grid>();
+        
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Vector3Int cell = new Vector3Int(i, 0, j);
+                Vector3 cellCenter = grid.GetCellCenterWorld(cell);
+                GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = cellCenter;
+            }
+        }
+    }
+}
