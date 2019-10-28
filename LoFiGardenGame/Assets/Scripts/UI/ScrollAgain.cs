87ab6170c274ee81277dcea504a71ed0be31ef12
Eliot Cowley <eliot93@live.com>
2019-10-27 20:30:25 -0700

    Experimenting with scrolling

---
+++ LoFiGardenGame/Assets/Scripts/UI/ScrollAgain.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ScrollAgain : MonoBehaviour
+{
+    RectTransform scrollRectTransform;
+    RectTransform contentPanel;
+    RectTransform selectedRectTransform;
+    GameObject lastSelected;
+
+    void Start()
+    {
+        scrollRectTransform = GetComponent<RectTransform>();
+        contentPanel = GetComponent<ScrollRect>().content;
+    }
+
+    void Update()
+    {
+        // Get the currently selected UI element from the event system.
+        GameObject selected = EventSystem.current.currentSelectedGameObject;
+
+        // Return if there are none.
+        if (selected == null)
+        {
+            Debug.Log("Nothing selected");
+            return;
+        }
+        // Return if the selected game object is not inside the scroll rect.
+        if (selected.transform.parent != contentPanel.transform)
+        {
+            Debug.Log("Selected object not in content");
+            return;
+        }
+        // Return if the selected game object is the same as it was last frame,
+        // meaning we haven't moved.
+        if (selected == lastSelected)
+        {
+            //Debug.Log("Same as before");
+            return;
+        }
+
+        // Get the rect tranform for the selected game object.
+        selectedRectTransform = selected.GetComponent<RectTransform>();
+        // The position of the selected UI element is the absolute anchor position,
+        // ie. the local position within the scroll rect + its height if we're
+        // scrolling down. If we're scrolling up it's just the absolute anchor position.
+        float selectedPositionY = Mathf.Abs(selectedRectTransform.anchoredPosition.y) + selectedRectTransform.rect.height;
+
+        // The upper bound of the scroll view is the anchor position of the content we're scrolling.
+        float scrollViewMinY = contentPanel.anchoredPosition.y;
+        // The lower bound is the anchor position + the height of the scroll rect.
+        float scrollViewMaxY = contentPanel.anchoredPosition.y + scrollRectTransform.rect.height;
+
+        //Debug.Log($"selectedPositionY: {selectedPositionY}; scrollViewMaxY: {scrollViewMaxY}");
+        Debug.Log($"Mathf.Abs(selectedRectTransform.anchoredPosition.y): {Mathf.Abs(selectedRectTransform.anchoredPosition.y)}");
+
+        // If the selected position is below the current lower bound of the scroll view we scroll down.
+        if (selectedPositionY > scrollViewMaxY)
+        {
+            float newY = selectedPositionY - scrollRectTransform.rect.height;
+            contentPanel.anchoredPosition = new Vector2(contentPanel.anchoredPosition.x, newY);
+        }
+        // If the selected position is above the current upper bound of the scroll view we scroll up.
+        else if (Mathf.Abs(selectedRectTransform.anchoredPosition.y) < scrollViewMinY)
+        {
+            contentPanel.anchoredPosition = new Vector2(contentPanel.anchoredPosition.x, Mathf.Abs(selectedRectTransform.anchoredPosition.y));
+        }
+
+        lastSelected = selected;
+    }
+}
---
+++ LoFiGardenGame/Assets/Scripts/UI/ScrollWithKeyboard.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ScrollRect))]
+public class ScrollWithKeyboard : MonoBehaviour
+{
+    [SerializeField]
+    private Button defaultSelectedButton;
+
+    private ScrollRect scrollRect;
+    private float viewportHeight;
+    private float viewPortMaxY;
+    private float scrollRectHeight;
+    private float scrollRectMaxY;
+    private RectTransform scrollRectTransform;
+
+    private void Start()
+    {
+        scrollRect = GetComponent<ScrollRect>();
+        defaultSelectedButton.Select();
+        viewportHeight = scrollRect.viewport.rect.height;
+        viewPortMaxY = scrollRect.viewport.rect.yMax;
+        scrollRectTransform = scrollRect.transform.parent.GetComponent<RectTransform>();
+        scrollRectHeight = scrollRectTransform.rect.height;
+        scrollRectMaxY = scrollRectTransform.rect.yMax;
+    }
+
+    private void Update()
+    {
+        //Debug.Log($"scrollRectHeight: {scrollRectHeight}; scrollRectMaxY: {scrollRectMaxY}");
+
+        RectTransform selectedRect = EventSystem.current.currentSelectedGameObject.transform.parent.GetComponent<RectTransform>();
+        //float selectedY = Mathf.Abs(selectedRect.anchoredPosition.y) + selectedRect.rect.height;
+        float localY = selectedRect.localPosition.y;
+        float selectedRectAnchorPositionY = Mathf.Abs(selectedRect.anchoredPosition.y);
+        float selectedHeight = selectedRect.rect.height;
+
+        float worldY = selectedRect.position.y;
+        float scrollMaxY = scrollRectTransform.position.y - (scrollRectHeight / 4);
+        //Debug.Log($"worldY: {worldY}; scrollRectY: {scrollRectTransform.position.y}; scrollMaxY: {scrollMaxY}");
+
+        float selectedY = worldY - (selectedRect.rect.height / 2);
+
+        //Debug.Log($"localY: {localY}");
+        //Debug.Log($"selectedAnchorY: {selectedRectAnchorPositionY}; selectedHeight: {selectedHeight}");
+        //Debug.Log($"selectedY: {selectedY}; scrollRectMaxY: {scrollRectMaxY}");
+
+        Debug.Log($"selectedY: {selectedY}; scrollMaxY: {scrollMaxY}");
+
+        if (selectedY < scrollMaxY)
+        {
+            float newY = Mathf.Abs(selectedY) - Mathf.Abs(scrollMaxY);
+            //Debug.Log($"newY: {newY}");
+            scrollRect.viewport.anchoredPosition = new Vector2(scrollRectTransform.anchoredPosition.x, newY);
+            
+        }
+    }
+}
